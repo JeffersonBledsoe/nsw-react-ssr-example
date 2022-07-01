608a9bb5c5e7b8e7fcefd8bec610987fa85e676e
me@jeffersonbledsoe.com <me@jeffersonbledsoe.com>
2022-07-01 01:00:35 +0100

    Install nsw-design-system-react

---
+++ pages/_document.js
@@ -0,0 +1,22 @@
+import { Head, Html, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,400;0,700;1,400&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
